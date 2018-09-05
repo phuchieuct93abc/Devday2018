e5c013c67caf9d579cf806c8a99b2124b904de2a
[Pioneer] Lâm Phúc Hiếu <hieu.lam@axonactive.vn>
2018-09-05 18:08:29 +0700

    Update socketio

---
+++ webcam-transfer-learning/packman-server.js
@@ -0,0 +1,31 @@
+
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/packman-server/index.html');
+});
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+
+  });
+});
+
+var time=0;
+setInterval(function(){
+  io.emit('time', time++);
+
+},1000)
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
