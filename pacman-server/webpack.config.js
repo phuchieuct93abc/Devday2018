420166572ca80f791d902d85d36043608679bd17
hieu <phuchieuct93@gmail.com>
2018-10-03 19:27:34 +0700

    Auto stash before merge of "TestingPageForEachToken" and "SupportWebpackWatch"

--- pacman-server/webpack.config.js
+++ pacman-server/webpack.config.js
@@ -1,32 +1,83 @@
 const path = require('path');
 
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
-  },
-  watch:true,
+ 
   mode: 'development',
-  module: {
-    rules: [{
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: __dirname + '/src',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
-      }
-    ],
-  },
-  plugins: [
-    new ExtractTextPlugin("styles.css")
-  ]
+
 };
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const commonModule= {
+  rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: __dirname + '/src',
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader'
+      })
+    }
+  ],
+}
+module.exports = [{
+  entry: './pacman-server/src/testing.js',
+
+  output: {
+    filename: 'testing.js',
+    path: path.resolve(__dirname, './pacman-server/dist')
+  },
+  watch: true,
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+    new HtmlWebpackPlugin({
+      title: 'TestingPage', //Testing page for each team,
+      filename: 'testing.html',
+      template: './pacman-server/src/pages/testing.html'
+    })
+  ],
+  module:commonModule
+},
+{
+  entry: './pacman-server/src/admin.js',
+  output: {
+    filename: 'admin.js',
+    path: path.resolve(__dirname, './pacman-server/dist')
+  },
+  watch: true,
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+    new HtmlWebpackPlugin({
+      title: 'AdminPage', // Admin page for cofiguration
+      filename: 'admin.html',
+      template: './pacman-server/src/pages/admin.html'
+    }),
+  ],
+  module:commonModule
+
+},
+{
+  entry: './pacman-server/src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, './pacman-server/dist')
+  },
+  watch: true,
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+    new HtmlWebpackPlugin({
+      title: 'Main', //Main page for fighting
+      filename: 'main.html',
+      template: './pacman-server/src/pages/main.html'
+    })
+  ],
+  module:commonModule
+
+}
+]
