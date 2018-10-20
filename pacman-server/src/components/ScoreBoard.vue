fd9cdc8345f457247dd36ee9f107c4404cbf1065
Hung Nguyen <nthung.vlvn@gmail.com>
2018-10-20 22:56:56 +0700

    Add score board component

---
+++ pacman-server/src/components/ScoreBoard.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="score-board">
+        <div class="score-container">
+            <div class="score">{{ score }}</div>
+        </div>
+        <p class="team-name">{{ teamName }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class ScoreBoard extends Vue {
+        @Prop({default: 0}) private score!: number;
+        @Prop() private teamName!: String;
+    }
+</script>
+
+
+<style lang="less" scoped>
+    @axon-blue: #0096d7ff;
+
+    .score-board {
+        border: @axon-blue 2px dashed;
+        width: 150px;
+        height: 150px;
+    }
+
+    .score-container {
+        height: 75%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .score {
+            font-size: 80px;
+        }
+    }
+
+    .team-name {
+        height: 25%;
+        text-align: center;
+        font-size: large;
+    }
+</style>
