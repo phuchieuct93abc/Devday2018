6fe152a63d80e256fff765dba0d4eacec798244d
Hung Nguyen <nthung.vlvn@gmail.com>
2018-10-22 20:42:42 +0700

    Add vuetify to project

--- pacman-server/src/views/AdminPage.vue
+++ pacman-server/src/views/AdminPage.vue
@@ -5,7 +5,12 @@
 </template>
 
 <script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
 
+    @Component
+    export default class AdminPage extends Vue {
+
+    }
 </script>
 
 <style lang="less" scoped>
