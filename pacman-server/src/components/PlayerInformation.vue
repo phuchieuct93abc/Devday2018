c716f128d425362734322898fc3e24968e9f3cdc
Hung Nguyen <nthung.vlvn@gmail.com>
2018-10-24 06:48:24 +0700

    Add player register view in admin

---
+++ pacman-server/src/components/PlayerInformation.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container>
+        <v-layout>
+            <v-flex>
+                <v-text-field label="Id"></v-text-field>
+                <v-text-field label="Token"></v-text-field>
+                <v-text-field label="Name"></v-text-field>
+                <v-combobox label="Color"></v-combobox>
+                <v-text-field label="Score"></v-text-field>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class PlayerInformation extends Vue {
+
+    }
+</script>
+
+<style lang="less">
+
+</style>
--- pacman-server/src/views/Admin.vue
+++ pacman-server/src/views/Admin.vue
@@ -1,16 +1,32 @@
 <template>
-    <div class="admin-page">
-        <admin-panel></admin-panel>
-    </div>
+    <v-container>
+        <v-layout text-xs-center wrap>
+            <v-flex mb-4 xs12>
+                <h1 class="display-2 font-weight-bold mb-3">
+                    Welcome to Admin page
+                </h1>
+            </v-flex>
+
+            <v-flex mb-5 xs6>
+                <h2 class="headline font-weight-bold mb-3">Player 1</h2>
+                <player-information></player-information>
+            </v-flex>
+
+            <v-flex mb-5 xs6>
+                <h2 class="headline font-weight-bold mb-3">Player 2</h2>
+                <player-information></player-information>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from "vue-property-decorator";
-    import AdminPanel from "../components/AdminPanel.vue";
+    import PlayerInformation from "../components/PlayerInformation.vue";
 
     @Component({
         components: {
-            "admin-panel": AdminPanel
+            "player-information": PlayerInformation
         }
     })
     export default class Admin extends Vue {
